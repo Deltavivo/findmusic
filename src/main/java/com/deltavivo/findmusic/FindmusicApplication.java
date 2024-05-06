870031b3fafa7abe25d9873a3a056afb091e947b
Delfim Baltazar <delfim.baltazar@gmail.com>
2024-05-06 15:41:22 -0300

    start project to find musics

---
+++ src/main/java/com/deltavivo/findmusic/FindmusicApplication.java
@@ -0,0 +1,13 @@
+package com.deltavivo.findmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindmusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindmusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deltavivo/findmusic/FindmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deltavivo.findmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindmusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
